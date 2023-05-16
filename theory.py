35086b214ac4a9bacc2b8808dfd89432213f69b3
ashleyyi <ashley02.yi@gmail.com>
2023-05-16 16:29:57 -0400

    Fixed triad method

--- theory.py
+++ theory.py
@@ -78,7 +78,7 @@ class Theory:
             
         right_color = self.color_obj.hls[0] - 0.33, self.color_obj.hls[1], self.color_obj.hls[2]
         
-        if right_color < 0:
+        if right_color[0] < 0:
             right_color = 1 + right_color[0], right_color[1], right_color[2]
             
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(left_color), color.Color.hls_to_hex(right_color)
@@ -127,4 +127,4 @@ if __name__ == "__main__":
         
     test = Theory('#1ECBE1')
     
-    print(test.analogous())
+    print(test.triad())
