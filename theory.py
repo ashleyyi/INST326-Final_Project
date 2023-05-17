7e04f1f6247c1504ccc408d537999221ab31ff29
ashleyyi <ashley02.yi@gmail.com>
2023-05-17 12:12:46 -0400

    Added docstrings

--- theory.py
+++ theory.py
@@ -21,26 +21,39 @@ class Theory:
         self.color_obj = color.Color(self.hexcode)
             
     def monochromatic(self):
+        """Creates an alternate color based off of the hexcode attribute by adjusting the light value, 
+           following the monochromatic color theory.
         
+        Returns: hex code String of given hexcode
+                 hex code String of new_hls alternate color"""
         
+        # If light value is greater than or equal to 60%
         if self.color_obj.hls[1] >= 0.6:
             new_hls = self.color_obj.hls[0], self.color_obj.hls[1] - 0.1, self.color_obj.hls[2]
             
             return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(new_hls)
         
+        # If light value is less than 60%
         else:
             new_hls = self.color_obj.hls[0], self.color_obj.hls[1] + 0.1, self.color_obj.hls[2]
             
             return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(new_hls)
         
     def analogous(self):
-        # docstrings
+        """Creates two alternate colors based off of the hexcode attribute by adjusting the hue value, 
+           following the analogous color theory.
         
+        Returns: hex code String of given hexcode
+                 hex code String of left_color alternate color
+                 hex code String of right_color alternate color"""
+        
+        # Creating left color
         left_color = self.color_obj.hls[0] + 0.083, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if left_color[0] > 1:
             left_color = 1 - left_color[0], left_color[1], left_color[2]
-            
+        
+        # Creating right color
         right_color = self.color_obj.hls[0] - 0.083, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if right_color[0] < 0:
@@ -49,7 +62,11 @@ class Theory:
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(left_color), color.Color.hls_to_hex(right_color)
     
     def complementary(self):
-        # docstrings
+        """Creates an alternate color based off of the hexcode attribute by adjusting the hue value, 
+           following the complementary color theory.
+        
+        Returns: hex code String of given hexcode
+                 hex code String of second_color alternate color"""
         
         second_color = self.color_obj.hls[0] - 0.5, self.color_obj.hls[1], self.color_obj.hls[2]
         
@@ -59,18 +76,26 @@ class Theory:
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(second_color)
     
     def split_complementary(self):
-        # docstrings
+        """Creates two alternate colors based off of the hexcode attribute by adjusting the hue value, 
+           following the split complementary color theory.
         
+        Returns: hex code String of given hexcode
+                 hex code String of left_com alternate color
+                 hex code String of right_com alternate color"""
+        
+        # Create center point for opposing analogous colors
         com = self.color_obj.hls[0] - 0.5, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if com[0] < 0:
             com = 1 - com[0], com[1], com[2]
-            
+        
+        # Creating left color
         left_com = com[0] - 0.083, com[1], com[2]
         
         if left_com[0] < 0:
             left_com = 1 + left_com[0], left_com[1], left_com[2]
             
+        # Creating right color
         right_com = com[0] + 0.083, com[1], com[2]
         
         if right_com[0] > 1: 
@@ -79,13 +104,20 @@ class Theory:
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(left_com), color.Color.hls_to_hex(right_com)
     
     def triad(self):
-        # add docstrings
+        """Creates two alternate colors based off of the hexcode attribute by adjusting the hue value, 
+           following the triadic color theory.
         
+        Returns: hex code String of given hexcode
+                 hex code String of left_color alternate color
+                 hex code String of right_color alternate color"""
+        
+        # Creating left color
         left_color = self.color_obj.hls[0] + 0.33, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if left_color[0] > 1:
             left_color = 1 - left_color[0], left_color[1], left_color[2]
-            
+          
+        # Creating right color  
         right_color = self.color_obj.hls[0] - 0.33, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if right_color[0] < 0:
@@ -94,23 +126,34 @@ class Theory:
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(left_color), color.Color.hls_to_hex(right_color)
             
     def tetradic(self):
-        # add docstrings
+        """Creates four alternate colors based off of the hexcode attribute by adjusting the hue value, 
+           following the tetradic color theory.
         
+        Returns: hex code String of given hexcode
+                 hex code String of left alternate color
+                 hex code String of left_com alternate color
+                 hex code String of right alternate color
+                 hex code String of right_com alternate color"""
+        
+        # Creating left color
         left = self.color_obj.hls[0] - 0.083, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if left[0] < 0:
             left = 1 + left[0], left[1], left[2]
-            
+        
+        # Creating left complementary color   
         left_com = left[0] - 0.5, left[1], left[2]
         
         if left_com[0] < 0:
             left_com = 1 + left_com[0], left_com[1], left_com[2]
-            
+         
+        # Creating right color   
         right = self.color_obj.hls[0] + 0.083, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if right[0] > 1:
             right = 1 - right[0], right[1], right[2]
         
+        # Creating right complementary color
         right_com = right[0] - 0.5, right[1], right[2]
         
         if right_com[0] < 0:
@@ -119,18 +162,27 @@ class Theory:
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(left), color.Color.hls_to_hex(left_com), color.Color.hls_to_hex(right), color.Color.hls_to_hex(right_com)
     
     def square(self):
-        # add docstrings
+        """Creates three alternate colors based off of the hexcode attribute by adjusting the hue value, 
+           following the square color theory.
         
+        Returns: hex code String of given hexcode
+                 hex code String of dom_com alternate color
+                 hex code String of secondary alternate color
+                 hex code String of sec_com alternate color"""
+        
+        # Creating complementary color of given hexcode
         dom_com = self.color_obj.hls[0] - 0.5, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if dom_com[0] < 0:
             dom_com = 1 + dom_com[0], dom_com[1], dom_com[2]
         
+        # Creating secondary color
         secondary = self.color_obj.hls[0] - 0.25, self.color_obj.hls[1], self.color_obj.hls[2]
         
         if secondary[0] < 0:
             secondary = 1 + secondary[0], secondary[1], secondary[2]
-            
+         
+        # Creating complementary color of secondary   
         sec_com = secondary[0] - 0.5, secondary[1], secondary[2]
         
         if sec_com[0] < 0:
@@ -139,7 +191,9 @@ class Theory:
         return color.Color.hls_to_hex(self.color_obj.hls), color.Color.hls_to_hex(secondary), color.Color.hls_to_hex(dom_com), color.Color.hls_to_hex(sec_com)
     
     def color_theory(self):
-        # add docstrings
+        """Calls all color theory methods to organize into a String and print.
+        
+        Side effects: prints String of all Theory class methods into Terminal"""
         
         sub_theories = "Your color is " + self.hexcode + ", here are the alternate colors that you can use with this:"
         sub_theories += "\nMonochromatic: " + self.monochromatic()[1]
@@ -153,5 +207,6 @@ class Theory:
         print(sub_theories)
     
 if __name__ == "__main__":
+    """When called by command line, takes first argument and calls the Theory class."""
         
     Theory(sys.argv[1]).color_theory()
